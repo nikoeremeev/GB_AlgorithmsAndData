7e8f567f1e1eb9c3aac784ef7d5b013be28f2ad4
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-01-27 20:33:59 +0300

    Add insertSort method

--- src/seminar1/Sort.java
+++ src/seminar1/Sort.java
@@ -3,15 +3,16 @@ package seminar1;
 public class Sort {
 
     public static void main(String[] args) {
-        int[] array = new int[] {5,3,8,2,6,1,0,9,7,5};
-        directSort(array);
+        int[] array = new int[]{5, 3, 8, 2, 6, 1, 0, 9, 7, 5};
+        insertSort(array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
 
         }
     }
 
-    public static void bubbleSort(int[] array){
+    // сортировка пузырьком
+    public static void bubbleSort(int[] array) {
         boolean finish;
         do {
             finish = true;
@@ -23,21 +24,35 @@ public class Sort {
                     finish = false;
                 }
             }
-        }while(!finish);
+        } while (!finish);
     }
 
-    public static void directSort(int[] array){
-        for (int i = 0; i < array.length-1; i++) {
+    // сортировка выбором
+    public static void directSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
             int minPosition = i;
-            for (int j = i+1; j < array.length; j++) {
-                if (array[j] < array[minPosition]){
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minPosition]) {
                     minPosition = j;
                 }
             }
-            if (i != minPosition){
+            if (i != minPosition) {
                 int temp = array[i];
                 array[i] = array[minPosition];
                 array[minPosition] = temp;
+            }
+        }
+    }
+
+    //сортировка вставками
+    public static void insertSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
             }
         }
     }
